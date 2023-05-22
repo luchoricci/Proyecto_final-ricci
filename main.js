cc4cb8d91df37cf8e9550100f1385a4f6e2b9481
luchoricci <lnr_2010@hotmail.com>
2023-05-22 19:43:03 -0300

    preentrega3

--- main.js
+++ main.js
@@ -1,97 +1,309 @@
+/iintento de nav responsive/
 
-alert("elegi las bebidas que quieras llevar, para salir ingrese 0")
-let elegirproductos = Number(prompt("1-vinos $10000 2-gin $30000 3-vodka $10000 4-ron $15000 5-licor $5000"))
-let seleccionarcantidad;
-let total = 0;
 
-const cantidad = (cant, precio) => { return cant * precio }
+const bar = document.getElementById("barra");
+const cerrar = document.getElementById("cerrar");
+const nav = document.getElementById("nav");
 
-while (elegirproductos != 0) {
-    switch (elegirproductos) {
-        case 1:
-            seleccionarcantidad = Number(prompt("el producto seleccionado es vinos, selecionar la cantidad"))
-            total += cantidad(seleccionarcantidad, 10000)
-            break;
-        case 2:
-            seleccionarcantidad = Number(prompt("el producto seleccionado es gin, selecionar la cantidad"))
-            total += cantidad(seleccionarcantidad, 30000)
-            break;
-        case 3:
-            seleccionarcantidad = Number(prompt("el producto seleccionado es vodka, selecionar la cantidad"))
-            total += cantidad(seleccionarcantidad, 10000)
-            break;
-        case 4:
-            seleccionarcantidad = Number(prompt("el producto seleccionado es ron, selecionar la cantidad"))
-            total += cantidad(seleccionarcantidad, 15000)
-            break;
+if (barra) {
+    bar.addEventListener("click", () => {
+        nav.classList.add("active")
 
-        case 5:
-            seleccionarcantidad = Number(prompt("el producto seleccionado es licor, selecionar la cantidad"))
-            total += cantidad(seleccionarcantidad, 5000)
-            break;
+    })
 
-        default:
-            break;
+
+}
+if (cerrar) {
+    cerrar.addEventListener("click", () => {
+        nav.classList.remove("active")
+
+    })
+
+
+}
+
+/array de productos/
+
+const productos =
+    [
+        {
+            nombre: "Budweiser",
+            tipo: "Cerveza",
+            imagen :"./imagen/budweiser.jpeg",
+            precio: 600
+        },
+
+        {
+            nombre: "Heineken",
+            tipo: "Cerveza",
+            imagen :"./imagen/heineken.jpeg",
+            precio: 500
+        },
+
+        {
+            nombre: "Stella Artois",
+            imagen :"./imagen/stella-artois.webp",
+            tipo: "Cerveza",
+            precio: 500
+        },
+
+        {
+            nombre: "Corona",
+            tipo: "Cerveza",
+            imagen :"./imagen/corona.jpeg",
+            precio: 600
+        },
+
+        {
+            nombre: "Ginness",
+            tipo: "Cerveza",
+            imagen :"./imagen/ginness.jpeg",
+            precio: 500
+        },
+
+        {
+            nombre: "Snow",
+            tipo: "Cerveza",
+            imagen :"./imagen/SNOW.webp",
+            precio: 500
+
+        },
+
+        {
+            nombre: "Jägermeister",
+            tipo: "Licor",
+            imagen :"./imagen/JAGERMEISTER.webp",
+            precio: 1500
+        },
+
+        {
+            nombre: "Baileys",
+            tipo: "Licor",
+            imagen :"./imagen/BAILEYS.webp",
+            precio: 1500
+        },
+        {
+            nombre: "Khlibniy",
+            tipo: "Vodka",
+            imagen :"./imagen/KHLIBNIY.webp",
+            precio: 2500
+        },
+
+        {
+            nombre: "Jack Daniels",
+            tipo: "Whisky",
+            imagen :"./imagen/JACK-DANIELS.webp",
+            precio: 2500
+        },
+
+        {
+            nombre: "Absolut",
+            tipo: "Vodka",
+            imagen :"./imagen/ABSOLUT.webp",
+            precio: 1500
+        },
+
+        {
+            nombre: "Captain Morgan ",
+            tipo: "Ron",
+            imagen :"./imagen/CAPTAIN-MORGAN.webp",
+            precio: 3500
+        },
+
+        {
+            nombre: "Red Star Er Guo",
+            tipo: "Vodka",
+            imagen :"./imagen/RED-STAR.webp",
+            precio: 2500
+        },
+
+        {
+            nombre: "Bagpiper United Spirits",
+            tipo: "Whisky",
+            imagen :"./imagen/BAGPIPER.webp",
+            precio: 3500
+        },
+
+        {
+            nombre: "McDowell ‘s",
+            tipo: "Whisky",
+            imagen :"./imagen/MCDOWELLS.webp",
+            precio: 2500
+        },
+
+        {
+            nombre: "Officer ‘s ",
+            tipo: "Whisky",
+            imagen :"./imagen/OFFICER.webp",
+            precio: 4500
+        },
+
+        {
+            nombre: "Johnnie Walker",
+            tipo: "Whisky",
+            imagen :"./imagen/johnnie-walker.jpg",
+            precio: 5500
+        },
+
+        {
+            nombre: "Pirassunga 51 ",
+            tipo: "Cachaça",
+            imagen :"./imagen/PIRASSUGA-51.webp",
+            precio: 3500
+        },
+
+        {
+            nombre: "Tanduay ",
+            tipo: "Ron",
+            imagen :"./imagen/TANDUAY.webp",
+            precio: 5500
+        },
+
+        {
+            nombre: "Bacardi",
+            tipo: "Ron",
+            imagen :"./imagen/BACARDI.webp",
+            precio: 2500
+        },
+
+
+        {
+            nombre: "Emperador ",
+            tipo: "Brandy",
+            imagen :"./imagen/EMPERADOR.webp",
+            precio: 5500
+        },
+
+        {
+            nombre: "San Miguel ",
+            tipo: "Ginebra",
+            imagen :"./imagen/SAN-MIGUEL.webp",
+            precio: 4500
+        },
+        {
+            nombre: "Soju Chum Churum ",
+            tipo: "Soju",
+            imagen :"./imagen/CHUM-CHURUM.webp",
+            precio: 3500
+        },
+
+        {
+            nombre: "Smirnoff",
+            tipo: "Vodka",
+            imagen :"./imagen/SMIRNOFF.webp",
+            precio: 1500
+        },
+
+
+        {
+            nombre: "Jinro",
+            tipo: "Soju",
+            imagen :"./imagen/JINRO.webp",
+            precio: 5500
+        }
+    ]
+
+    const contenedorproductos = document.querySelector("#produc-list");
+const botonmenu = document.querySelectorAll(".boton-menu")
+
+
+
+    function cargarproductos(productos) {
+
+        contenedorproductos.innerHTML="";
+
+     
+
+        productos.forEach(producto => {
+
+            const div = document.createElement("div");
+            div.classList.add("producto");
+            div.innerHTML =`<img src="${producto.imagen}" alt="${producto.nombre}">
+            <div class="tipo-producto">
+                <span>${producto.nombre}</span>
+                <h5>${producto.tipo}</h5>
+                <div class="estrellas">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <h4>
+                ${producto.precio}
+                </h4>
+            </div>
+            <button class="producto-agregar"><i class="fas fa-cart-shopping carrito-produc"></i></button>
+    
+            `;
+            contenedorproductos.append(div)
+            
+        })
 
     }
-    elegirproductos = Number(prompt("1-vinos $10000 2-gin $30000 3-vodka $10000 4-ron $15000 5-licor"))
+    
 
-}
+    cargarproductos(productos);
 
-alert("el total de tu compra es igual a: " + total)
+botonmenu.forEach(boton => {   
+    boton.addEventListener("click",(e) => {
+       
+        if (e.currentTarget.id !="todos") {
 
-
-if (total >= 50000) {
-    alert("el envio es gratuito")
-}
-else { alert(" el costo del envio es de 4000, el total es: " + total) }
-
-const productosarray = [{ nombre: "Budweiser", tipo: "Cerveza", precio: "600" },
-{ nombre: "Heineken", tipo: "Cerveza", precio: "500" },
-{ nombre: "Stella Artois", tipo: "Cerveza", precio: "500" },
-{ nombre: "Corona", tipo: "Cerveza", precio: "600" },
-{ nombre: "Ginness", tipo: "Cerveza", precio: "500" },
-{ nombre: "Snow", tipo: "Cerveza", precio: "500" },
-{ nombre: "Jägermeister", tipo: "Licor", precio: "1500" },
-{ nombre: "Baileys", tipo: "Licor", precio: "1500" },
-{ nombre: "Khlibniy", tipo: "Vodka", precio: "2500" },
-{ nombre: "Jack Daniels", tipo: "Whiskey", precio: "2500" },
-{ nombre: "Absolut", tipo: "Vodka", precio: "1500" },
-{ nombre: "Captain Morgan ", tipo: "Ron", precio: "3500" },
-{ nombre: "Red Star Er Guo", tipo: "Vodka", precio: "2500" },
-{ nombre: "Bagpiper United Spirits", tipo: "Whisky", precio: "3500" },
-{ nombre: "McDowell ‘s", tipo: "Whisky", precio: "2500" },
-{ nombre: "Officer ‘s ", tipo: "Whisky", precio: "4500" },
-{ nombre: "Johnnie Walker", tipo: "Whisky", precio: "5500" },
-{ nombre: "Pirassunga 51 ", tipo: "Cachaça", precio: "3500" },
-{ nombre: "Tanduay ", tipo: "Ron", precio: "5500" },
-{ nombre: "Bacardi", tipo: "Ron", precio: "2500" },
-{ nombre: "Emperador ", tipo: "Brandy", precio: "5500" },
-{ nombre: "San Miguel ", tipo: "Ginebra", precio: "4500" },
-{ nombre: "Soju Chum Churum ", tipo: "Soju", precio: "3500" },
-{ nombre: "Smirnoff", tipo: "Vodka", precio: "1500" },
-{ nombre: "Jinro", tipo: "Soju", precio: "5500" }
-]
-
-
-for (let i = 0; i < productosarray.length; i++){
-    console.log("=======================")
-    console.log("nombre: "+ productosarray[i].nombre);
-console.log("tipo: "+ productosarray[i].tipo);
-console.log("precio: "+ productosarray[i].precio);
-
-}
-
-const cerveza = productosarray.slice(0,6)
-
-for (let i=0; i < cerveza.length; i++){
-    console.log("=======================")
-console.log("nombre: "+ cerveza[i].nombre);
-console.log("tipo: "+ cerveza[i].tipo);
-console.log("precio: "+ cerveza[i].precio);
-}
+      
+        const productosboton = productos.filter(producto => producto.tipo === e.currentTarget.id);
+        cargarproductos(productosboton);
+    } else(cargarproductos(productos))
+      })
+})
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
